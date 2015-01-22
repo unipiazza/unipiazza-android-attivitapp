ae9882e3fcc8e7c1c8436acbcfff7ca89fc902bb
monossido <monossido@lorenzobraghetto.com>
2015-01-22 22:56:17 +0100

    bottone gift disattivato se filteredPrize == 0

--- app/src/main/java/com/unipiazza/attivitapp/ui/HomeActivity.java
+++ app/src/main/java/com/unipiazza/attivitapp/ui/HomeActivity.java
@@ -18,166 +18,169 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.unipiazza.attivitapp.CurrentShop;
 import com.unipiazza.attivitapp.CurrentUser;
 import com.unipiazza.attivitapp.JSONParser;
 import com.unipiazza.attivitapp.R;
 
 public class HomeActivity extends Activity {
-	JSONParser jParser = new JSONParser();
-	// url per la ricerca dell'ID
-	ImageButton btnAddProduct;
-	ImageButton btnGift;
-	private NfcAdapter mNfcAdapter;
-	private String[][] techListsArray;
-	private PendingIntent pendingIntent;
-	public static final String MIME_TEXT_PLAIN = "text/plain";
+    JSONParser jParser = new JSONParser();
+    // url per la ricerca dell'ID
+    ImageButton btnAddProduct;
+    ImageButton btnGift;
+    private NfcAdapter mNfcAdapter;
+    private String[][] techListsArray;
+    private PendingIntent pendingIntent;
+    public static final String MIME_TEXT_PLAIN = "text/plain";
 
-	// Classe di controllo connessione
-	public class ConnectionDetector {
-		private Context _context;
+    // Classe di controllo connessione
+    public class ConnectionDetector {
+        private Context _context;
 
-		public ConnectionDetector(Context context) {
-			this._context = context;
-		}
+        public ConnectionDetector(Context context) {
+            this._context = context;
+        }
 
-		public boolean isConnectingToInternet() {
-			ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
-			if (connectivity != null) {
-				NetworkInfo[] info = connectivity.getAllNetworkInfo();
-				if (info != null)
-					for (int i = 0; i < info.length; i++)
-						if (info[i].getState() == NetworkInfo.State.CONNECTED) {
-							return true;
-						}
-			}
-			return false;
-		}
-	}
+        public boolean isConnectingToInternet() {
+            ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivity != null) {
+                NetworkInfo[] info = connectivity.getAllNetworkInfo();
+                if (info != null)
+                    for (int i = 0; i < info.length; i++)
+                        if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                            return true;
+                        }
+            }
+            return false;
+        }
+    }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.home);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-		CurrentUser currentUser = CurrentUser.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        CurrentUser currentUser = CurrentUser.getInstance();
 
-		String user = currentUser.getFirst_name();
-		String user_lastname = currentUser.getLast_name().substring(0, 1);
+        String user = currentUser.getFirst_name();
+        String user_lastname = currentUser.getLast_name().substring(0, 1);
 
-		int coins = currentUser.getTotal_coins();
+        int coins = currentUser.getTotal_coins();
 
-		// Buttoni
-		ImageButton btnAddProduct = (ImageButton) findViewById(R.id.add_coin);
-		ImageView avatar = (ImageView) findViewById(R.id.avatar);
-		if (!currentUser.isGender())
-			avatar.setImageResource(R.drawable.user_icon_m);
-		else
-			avatar.setImageResource(R.drawable.user_icon_f);
+        // Buttoni
+        ImageButton btnAddProduct = (ImageButton) findViewById(R.id.add_coin);
+        ImageView avatar = (ImageView) findViewById(R.id.avatar);
+        if (!currentUser.isGender())
+            avatar.setImageResource(R.drawable.user_icon_m);
+        else
+            avatar.setImageResource(R.drawable.user_icon_f);
 
-		btnAddProduct.setBackground(null);
-		ImageButton btnGift = (ImageButton) findViewById(R.id.gift_list);
-		btnGift.setBackground(null);
-		final TextView text_name = (TextView) findViewById(R.id.nicknameview);
-		final TextView last_name_view = (TextView) findViewById(R.id.lastnameview);
-		final TextView view_saldo = (TextView) findViewById(R.id.saldoview);
-		text_name.setText(user);
-		last_name_view.setText(user_lastname);
-		view_saldo.setText(coins + "");
-		Log.d("Utente Trovato, NOME --> ", user);
-		Log.d("Utente Trovato, COGNOME --> ", user_lastname);
+        btnAddProduct.setBackground(null);
+        ImageButton btnGift = (ImageButton) findViewById(R.id.gift_list);
+        btnGift.setBackground(null);
+        if (CurrentShop.getInstance().getFilteredPrizes(CurrentUser.getInstance().getTotal_coins()).size() == 0)
+            btnGift.setEnabled(false);
+        final TextView text_name = (TextView) findViewById(R.id.nicknameview);
+        final TextView last_name_view = (TextView) findViewById(R.id.lastnameview);
+        final TextView view_saldo = (TextView) findViewById(R.id.saldoview);
+        text_name.setText(user);
+        last_name_view.setText(user_lastname);
+        view_saldo.setText(coins + "");
+        Log.d("Utente Trovato, NOME --> ", user);
+        Log.d("Utente Trovato, COGNOME --> ", user_lastname);
 
-		btnAddProduct.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
+        btnAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-				//Controllo connessione ad Internet
-				ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
-				Boolean internet = cd.isConnectingToInternet();
+                //Controllo connessione ad Internet
+                ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
+                Boolean internet = cd.isConnectingToInternet();
 
-				if (internet == false)
-					Toast.makeText(HomeActivity.this, "Non c'è connessione ad internet =(\r\nRiprova fra qualche minuto!", Toast.LENGTH_SHORT).show();
-				else {
-					Intent i = new Intent(getApplicationContext(), AddCoins.class);
-					startActivity(i);
-				}
-			}
-		});
-		btnGift.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
+                if (internet == false)
+                    Toast.makeText(HomeActivity.this, "Non c'è connessione ad internet =(\r\nRiprova fra qualche minuto!", Toast.LENGTH_SHORT).show();
+                else {
+                    Intent i = new Intent(getApplicationContext(), AddCoins.class);
+                    startActivity(i);
+                }
+            }
+        });
+        btnGift.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-				//Controllo connessione ad Internet
-				ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
-				Boolean internet = cd.isConnectingToInternet();
+                //Controllo connessione ad Internet
+                ConnectionDetector cd = new ConnectionDetector(getApplicationContext());
+                Boolean internet = cd.isConnectingToInternet();
 
-				if (internet == false)
-					Toast.makeText(HomeActivity.this, "Non c'� connessione ad internet =(\r\nRiprova fra qualche minuto!", Toast.LENGTH_SHORT).show();
-				else {
-					Intent i = new Intent(getApplicationContext(), GiftActivity.class);
-					startActivity(i);
-				}
-			}
-		});
+                if (internet == false)
+                    Toast.makeText(HomeActivity.this, "Non c'è connessione ad internet =(\r\nRiprova fra qualche minuto!", Toast.LENGTH_SHORT).show();
+                else {
+                    Intent i = new Intent(getApplicationContext(), GiftActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
 
-		prepareNFCIntercept();
-	}
+        prepareNFCIntercept();
+    }
 
-	/**
-	 * Background Async Task per cercare l'ID facendo una chiamata HTTP
-	 * */
-	class LoadAllProducts extends AsyncTask<String, String, String> {
-		@Override
-		protected void onPreExecute() {
-			super.onPreExecute();
-		}
+    /**
+     * Background Async Task per cercare l'ID facendo una chiamata HTTP
+     */
+    class LoadAllProducts extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
 
-		@Override
-		protected String doInBackground(String... args) {
-			return null;
-		}
-	}
+        @Override
+        protected String doInBackground(String... args) {
+            return null;
+        }
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			finish();
-			break;
-		default:
-			break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	@Override
-	public void onBackPressed() {
-		Intent i = new Intent(HomeActivity.this, HomeTap.class);
-		Log.v("action ", "Tasto indietro premuto");
-		startActivity(i);
-		finish();
-	}
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(HomeActivity.this, HomeTap.class);
+        Log.v("action ", "Tasto indietro premuto");
+        startActivity(i);
+        finish();
+    }
 
-	public void onPause() {
-		super.onPause();
-		mNfcAdapter.disableForegroundDispatch(this);
-	}
+    public void onPause() {
+        super.onPause();
+        mNfcAdapter.disableForegroundDispatch(this);
+    }
 
-	public void onResume() {
-		super.onResume();
-		mNfcAdapter.enableForegroundDispatch(this, pendingIntent, null, techListsArray);
-	}
+    public void onResume() {
+        super.onResume();
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, null, techListsArray);
+    }
 
-	public void onNewIntent(Intent intent) {
-		Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-		//do nothing
-		Log.v("UNIPIAZZA", "onNewIntent");
-	}
+    public void onNewIntent(Intent intent) {
+        Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        //do nothing
+        Log.v("UNIPIAZZA", "onNewIntent");
+    }
 
-	private void prepareNFCIntercept() {
-		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-		pendingIntent = PendingIntent.getActivity(
-				this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+    private void prepareNFCIntercept() {
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 
-		techListsArray = new String[][] {};
-	}
+        techListsArray = new String[][]{};
+    }
 }
